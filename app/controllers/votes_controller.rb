63e221d56c8f2071930640e49578e4b506363771
arikarim <arikarim146@gmail.com>
2021-04-08 10:39:36 +0300

    Add redirect  to votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,8 +2,9 @@ class VotesController < ApplicationController
   before_action :authenticate_user!
   def create
     @vote = current_user.votes.build(article_id: params[:article_id])
+    @article = Article.find_by(id: @vote.article.id)
     if @vote.save
-      redirect_to root_path
+      redirect_to category_path(@article.category)
       flash[:notice] = 'You Voted this article'
     else
       flash[:notice] = 'Somthing happened'
@@ -12,10 +13,10 @@ class VotesController < ApplicationController
 
   def destroy
     vote = Vote.find_by(user: current_user, article_id: params[:article_id])
-
+    @article = Article.find_by(id: vote.article.id)
     if vote
       vote.destroy
-      redirect_to categories_path
+      redirect_to category_path(@article.category)
       flash[:notice] = 'You Unvoted this article'
     else
       redirect_to root_path
