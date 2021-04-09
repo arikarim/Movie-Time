5269f77f7a3a6c4c5258dc1ad88d26cd024b4bfa
arikarim <arikarim146@gmail.com>
2021-04-09 19:55:34 +0300

    Deployment

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,3 +4,8 @@ class ImageUploader < Shrine
     validate_max_size 1 * 1024 * 1024
   end
 end
+# storage :file
+
+# def store_dir
+#   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+# end
