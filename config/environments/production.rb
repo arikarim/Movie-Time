9f3eadb87f1db08643b94d3c051a48833974dc0c
arikarim <arikarim146@gmail.com>
2021-04-09 23:41:46 +0300

    deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.serve_static_assets = true
+  config.assets.compile = true
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
